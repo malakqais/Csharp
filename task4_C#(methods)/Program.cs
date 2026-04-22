09641782410cef3fc06748de0341304197d8e110
malakqais <fzgwmk6s97@privaterelay.appleid.com>
2026-04-23 00:47:34 +0300

    Add files via upload

---
+++ task4_C#(methods)/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4_C__methods_
+{
+    class test
+    {
+
+        //1
+        public void iseven(int num)
+        {
+            if(num%2==0) 
+            {
+                Console.WriteLine($"The number {num} is even");
+            }
+
+            else 
+            {
+                Console.WriteLine($"The number {num} is odd");
+            }
+            
+
+        }
+
+
+        //2
+
+        public void second_smallest(int[] nums,int size)
+         {
+
+            Array.Sort(nums);
+            //int first = nums[0];
+            //int second = nums[1];
+            //for (int i = 0; i < size-1; i++)
+            //{
+
+            //    for (int j = i+1; i < size; i++)
+            //    {
+            //        if (nums[j]<first)
+            //            first = nums[j];
+            //        else if (nums[j]<second) second = nums[j];
+
+            //    }
+            //}
+
+            Console.WriteLine($"Output:{nums[1]}");
+        }
+
+
+        //3
+
+
+        public int factorial(int number)
+        {
+            int fact = 1;
+            for (int i = 0; i < number; i++)
+            {
+
+               fact *= (number-i);
+
+
+            }
+        return fact;
+        }
+
+
+        //5
+        public int find_largest(int[] nums, int size)
+        {
+
+            int largest=nums[0];
+            foreach (int i in nums)
+            {
+                if (i > largest)
+                { largest = i; }
+
+
+            }
+          return largest;
+
+        }
+
+
+        //6
+        public void print(int n)
+        {
+            int number = 1;
+
+            for (int i = 1; i <= n; i++) 
+            {
+                for (int j = 1; j <= i; j++) 
+                {
+                    Console.Write(number + " ");
+                    number++;
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            test t=new test();
+            //2
+            int[] nums = { 4, -3, 7, 2, 0 };
+            t.second_smallest(nums,nums.Length);
+
+
+            //3
+            Console.WriteLine("input a number to calculate the factorial");
+           int number=int.Parse( Console.ReadLine());
+            int fact=t.factorial(number);
+            Console.WriteLine($"Output: {fact} ({number} × {number-1} ×{number - 2} × {number - 3} × 1 = {fact})");
+
+
+            //5
+            int[] nums1 = { 3, 1, 4, 1, 5, 9 };
+            int largest=t.find_largest(nums1,nums1.Length);
+            Console.WriteLine($" Output:{largest}");
+
+
+                //6
+                t.print(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ task4_c#/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4_c_
+{
+
+    class student
+    {
+        string name;
+        int age;
+        double grade;
+        const string universityName = "Yarmouk University";
+        public static int studentCount;
+
+
+       public student()
+        {  studentCount ++; }
+       public void DisplayInfo()
+        {
+            Console.WriteLine($" student name :{name}");
+            Console.WriteLine($" student name :{age}");
+            Console.WriteLine($" student name :{grade}");
+            Console.WriteLine($" student name :{universityName}");
+        
+        }
+
+        public void UpdateGrade(double newGrade)
+        {
+
+            grade = newGrade;
+        }
+        public bool IsPassed()
+        {
+            if (grade >= 50)
+            { return true; }
+        
+            return false;
+        
+        }
+
+
+        public void set(string n, int a, double g)
+        { 
+        name=n; age=a; grade = g; 
+        
+        
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student S1=new student();
+            student S2 = new student();
+            student S3 = new student();
+
+            S1.set("malak", 22, 100);
+            S2.set("haya", 23, 90);
+            S3.set("rayan", 24, 80);
+
+            S1.DisplayInfo();
+            S2.DisplayInfo();
+            S3.DisplayInfo();
+
+            S1.UpdateGrade(90);
+            S2.UpdateGrade(80);
+            S3.UpdateGrade(70);
+
+
+            Console.WriteLine($"is student 1 passed? {S1.IsPassed()} ");
+            Console.WriteLine($"is student 2 passed? {S2.IsPassed()} ");
+            Console.WriteLine($"is student 3 passed? {S3.IsPassed()} ");
+            Console.WriteLine($"total number of student is  {student.studentCount} ");
+
+
+
+
+            /*
+             
+             What is a class : defining a new data type to use it in the program later insted of creating the fields every time when you need to use it 
+             What is an object : is an instant of the class 
+             Example from their code : class->student & object->S1/S2/S3
+             
+             */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
